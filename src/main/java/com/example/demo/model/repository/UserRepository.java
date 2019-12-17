414921e4a528a9c78a672f01efa218a3b73f03e2
GUTINHLIAO <gutingliao@163.com>
2019-12-17 23:07:51 +0800

    添加基础依赖 完成数据库配置

---
+++ src/main/java/com/example/demo/model/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.model.repository;
+
+import com.example.demo.model.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
